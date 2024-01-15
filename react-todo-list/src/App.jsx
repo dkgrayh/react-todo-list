76b9993f8f90577d8dc77a70f9b7d3d8c11ab621
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2024-01-15 05:10:23 -0500

    added form and todo list

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,7 +1,42 @@
-import './App.css';
+import { useState } from 'react';
+import './styles.css';
 
 function App() {
-  return <></>;
+  const [newItem, setNewItem] = useState('');
+
+  const handleChange = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={handleChange}
+        className="new-item-form"
+      >
+        <div className="form-row">
+          <label htmlFor="">New Item</label>
+          <input
+            value={newItem}
+            onChange={(event) => setNewItem(event.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
